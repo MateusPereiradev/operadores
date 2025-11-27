063c9e97c3b51b251e849effbc2479bfa6a12e72
MateusPereiradev <dev.mateuspereira@gmail.com>
2025-11-27 19:14:53 -0300

    Prazo e avista

--- prazoavista.py
+++ prazoavista.py
@@ -1,6 +1,8 @@
 produto= float(input('Digite aqui o valor do produto:'))
-desconto= produto*5/100
-prazo= produto*8/100
-print(f'Referente ao preço do produto {produto:.2f} o valor com 5% de desconto é {produto-desconto:.2f}')
-print(f'Referente ao preço do produto {produto:.2f} o valor com 8% de acrescimo é {produto+prazo:.2f}')
+avista= produto-produto*0.05
+prazo= produto+produto*0.08
+print(f'Referente ao valor do produto o mesmo com 5% de desconto é R${avista:.2f}!')
+print(f'Referente ao valor do produto o mesmo com 8% de juros a prazo é R${prazo:.2f}!')
+
+
 # mostre o valor de um produto com 5% de desconto e outro valor a prazo com 8% de acréscimo em cima do valor do produto.
