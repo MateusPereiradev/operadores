a05208e8769143e3014f6b2168c02c029272540f
MateusPereiradev <dev.mateuspereira@gmail.com>
2025-11-27 18:59:39 -0300

    Sucessor e Antecessor

--- desafiosucessor.py
+++ desafiosucessor.py
@@ -1,5 +1,5 @@
-num= int(input('Digite aqui um número:'))
-print(f'O sucessor de {num} é {num+1}')
-print(f'O antecessor de {num} é {num-1}')
+num= int(input('Digite aqui algum número inteiro:'))
+print(f'Referente ao número {num}, seu sucessor é {num+1}')
+print(f'Referente ao número {num}, seu antecessor é {num-1}')
 
 # faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor
