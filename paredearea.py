fd9d94fd7dbec3f3b2c0dfd72e4ea15a52d8bb8e
MateusPereiradev <dev.mateuspereira@gmail.com>
2025-11-27 19:12:22 -0300

    Pintar área da parede

--- paredearea.py
+++ paredearea.py
@@ -1,9 +1,10 @@
-larg= float(input('Digite aqui a largura da parede:'))
-alt= float(input('Digite aqui a altura da parede:'))
-area= larg*alt
-print(f'Sendo a largura da parede {larg} e a altura {alt} a area em metros quadros é {area:.2f}m²')
-tinta=area/2
-print(f'Sendo a area da parede {area}m² será preciso {tinta}L de tinta para pintar a parede toda.')
+largura= float(input('Digite aqui a largura da parede:'))
+altura= float(input('Digite aqui a altura da parede:'))
+area= largura * altura
+print(f'Referente a largura da parede de {largura} e a altura de {altura} a área da parede é {area}')
+tinta= area/2
+print(f'Referente a área da parede de {area}m² será necessário {tinta}L de tinta para pintar a parede!')
+
 # Faça um programa que leia a largura e a altura de uma parede em metro.
 # Calcule a sua área e a quantidade de tinta necessária para pintá-la
 # Sabendo que cada litro de tinta, pinta uma área de 2m².
